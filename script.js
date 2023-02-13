a7b97bf69b87007a9b759c3b9629b4c09c4d04b3
Amelss <ameleyka@gmail.com>
2023-02-13 15:18:29 +0000

    Added button changed orange once clicked

--- script.js
+++ script.js
@@ -3,39 +3,37 @@ let submitBtn = document.getElementById('submit-btn');
 let ratingsCard = document.querySelector(".rating-card");
 let thankYouCard = document.querySelector(".thank-you-card");
 let ratingNumber = document.getElementById("rating-number")
-let selection = document.querySelector("#selection")
 let result = document.querySelectorAll(".rating-btn")
 
- 
+
 
 for (let i = 0; i < ratings.length; i++){
     ratings[i].addEventListener('click', function (e) {
-        ratings[i].style.backgroundColor = "hsl(25, 97%, 53%)"; 
-        ratings[i].style.color = "white"; 
-        ratings[i].classList.toggle('selected')
-        console.log('Ratings clicked');
-
+        console.log('ratings clicked')
         result = e.target.innerHTML
-        console.log(result)
-        
-        
-       
     })
 }
+ratings.forEach(rated => {
+    rated.addEventListener('click', function () {
+        buttonChange(rated)
+    })
+})
 
-
-
+function buttonChange(a) {
+    ratings.forEach(rated => {
+        rated.style.background = "#3e444b";
+    })
+        a.style.background = "hsl(25, 97%, 53%)";
+        a.style.color = "white"
+}
 
 submitBtn.addEventListener('click', function () {
     console.log('clicked');
     submitBtn.classList.toggle("submit-selected")
     ratingNumber.textContent = result
-  
     // ratingsCard.classList.add("hidden")
     // thankYouCard.classList.remove("hidden")
-   
-    
-    
+
 })
 
 
