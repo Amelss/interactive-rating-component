1b7448b0a50d874244c6f85585dfab71401383e7
Amelss <ameleyka@gmail.com>
2023-02-13 13:18:41 +0000

    Rating functionality added - styling to be done

--- script.js
+++ script.js
@@ -1,13 +1,45 @@
-let ratings = document.querySelectorAll('#rating-btn')
-let submitBtn = document.getElementById('submit-btn')
+let ratings = document.querySelectorAll('.rating-btn');
+let submitBtn = document.getElementById('submit-btn');
+let ratingsCard = document.querySelector(".rating-card");
+let thankYouCard = document.querySelector(".thank-you-card");
+let ratingNumber = document.getElementById("rating-number")
+let selection = document.querySelector("#selection")
+let result = document.querySelectorAll(".rating-btn")
+
+ 
 
 for (let i = 0; i < ratings.length; i++){
-    ratings[i].addEventListener('click', function () {
-    ratings[i].classList.add('selected')   
-    console.log('Ratings clicked');
-})
+    ratings[i].addEventListener('click', function (e) {
+        ratings[i].style.backgroundColor = "hsl(25, 97%, 53%)"; 
+        ratings[i].style.color = "white"; 
+        ratings[i].classList.toggle('selected')
+        console.log('Ratings clicked');
+
+        result = e.target.innerHTML
+        console.log(result)
+        
+        
+       
+    })
 }
+
+
+
 
 submitBtn.addEventListener('click', function () {
     console.log('clicked');
+    submitBtn.classList.toggle("submit-selected")
+    ratingNumber.textContent = result
+  
+    // ratingsCard.classList.add("hidden")
+    // thankYouCard.classList.remove("hidden")
+   
+    
+    
 })
+
+
+
+
+
+
